ea2cf9d9011855285fac7b9bb90afc531e9a5ebf
elenalynx <elnlynx@gmail.com>
2023-01-05 02:06:23 +0200

    add xhr request (part4) crud. end l.23

--- index.js
+++ index.js
@@ -5,51 +5,105 @@ const host = 'http://localhost:8081';
 document.addEventListener('DOMContentLoaded', () => {
     const list = document.querySelector('ul');
     const form = document.querySelector('form');
+    const editForm = document.querySelector('.edit-form');
+    let usersStorage = [];
+
+    function toggleEditForm() {
+        editForm.classList.toggle('d-none');
+    }
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
-        const body = JSON.stringify({
-            name: form.elements.name.value,
-            phoneNumber:form.elements.phoneNumber.value,
-        });
+        const  body = new FormData(form);
+
+        // так складніше виглядає
+        // const body = JSON.stringify({
+        //     name: form.elements.name.value,
+        //     phoneNumber: form.elements.phoneNumber.value,
+        // });
 
         request(`${host}/users`, {
             method: 'POST',
             body,
-            contentType: 'application/json',
+            // це при використанні json
+            // contentType: 'application/json',
         }).then((users) => {
-            list.innerHTML = '';
-            users.forEach(({ id, name, phoneNumber }) => {
-                const item = document.createElement('li');
-
-                item.innerHTML = `
-                    <h2>${name}</h2>
-                    <p>${phoneNumber}</p>
-                `;
-
-                list.append(item);
-            });
+            renderUsers(users);
+            usersStorage = users;
 
             form.reset();
         }).catch(err => {
             console.log(err.message);
         })
+    });
+
+    editForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const body = new FormData(editForm);
+        const { id } = editForm.dataset;
+
+        request(`${host}/users/${id}`, {
+            method: 'PUT',
+            body,
+        })
+            .then((users) => {
+                usersStorage = users;
+                renderUsers(users);
+                editForm.reset();
+                toggleEditForm();
+            });
     })
+
+    list.addEventListener('click', ({ target }) => {
+        if (target.dataset.action === 'delete') {
+            const { id } = target.closest('li').dataset;
+
+            request(`${host}/users/${id}`, {
+                method: 'DELETE',
+            })
+                .then((users) => {
+                    renderUsers(users);
+                    usersStorage = users;
+                })
+                .catch((err) => {
+                    console.log(err.message);
+                })
+        }
+
+        if (target.dataset.action === 'edit') {
+            const { id } = target.closest('li').dataset;
+            const { name, phoneNumber } = usersStorage.find((user) => user.id === +id);
+            toggleEditForm();
+            editForm.dataset.id = id;
+
+            editForm.elements.name.value = name;
+            editForm.elements.phoneNumber.value = phoneNumber;
+
+        }
+    });
 
     request(`${host}/users`)
         .then((users) => {
             // console.log(users);
             //тут виключити лоадер, якщо в нас є
+            renderUsers(users);
+            usersStorage = users;
+        })
+    function renderUsers(users) {
+        list.innerHTML = '';
 
-            users.forEach(({id, name, phoneNumber}) => {
-                const item = document.createElement('li');
+        users.forEach(({id, name, phoneNumber}) => {
+            const item = document.createElement('li');
+            item.dataset.id = id;
 
-                item.innerHTML = `
+            item.innerHTML = `
                     <h2>${name}</h2>
                     <p>${phoneNumber}</p>
+                    <button data-action="edit">R</button>
+                    <button data-action="delete">X</button>
                 `;
 
-                list.append(item);
-            });
-        })
+            list.append(item);
+        });
+    }
 });
--- server/server.mjs
+++ server/server.mjs
@@ -1,19 +1,22 @@
 import fastify from "fastify";
 import fastifyCors from "@fastify/cors";
-
+import fastifyMultipart from "@fastify/multipart";
 const server = fastify({
     logger: true,
 });
 
 server.register(fastifyCors);
-const users = [
+server.register(fastifyMultipart, {
+    addToBody: true,
+});
+
+let users = [
     {
         name: 'Olena',
         phoneNumber: '12343',
         id: 1,
     }
 ];
-
 server.get('/users', () => {
     return users;
 });
@@ -30,6 +33,22 @@ server.post('/users', (request) => {
     return users;
 })
 
+server.delete('/users/:id', (request) => {
+    const { id } = request.params;
+    users = users.filter((user) => user.id.toString() !== id);
+
+    return users;
+});
+
+server.put('/users/:id', (request) => {
+    const { id } = request.params;
+    const { name, phoneNumber } = request.body;
+    const user = users.find((user) => user.id.toString() === id);
+    user.phoneNumber = phoneNumber;
+    user.name = name;
+
+    return users;
+})
 
 server.listen({
     port: 8081,
